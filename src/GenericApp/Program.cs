9e792d4539c730d16a8ea35449855645032a8a5d
CR7MentalityN1 <nzaichenko531@gmail.com>
2026-04-16 14:51:22 +0300

    Demo problem without generics - code duplication

--- src/GenericApp/Program.cs
+++ src/GenericApp/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ========== ПЛОХОЙ КОД (БЕЗ GENERICS) ==========
+// Класс только для int
+class IntStorage
+{
+    private int value;
+    
+    public void SetValue(int val)
+    {
+        value = val;
+    }
+    
+    public int GetValue()
+    {
+        return value;
+    }
+}
+
+// Класс только для string (дублирование кода!)
+class StringStorage
+{
+    private string value;
+    
+    public void SetValue(string val)
+    {
+        value = val;
+    }
+    
+    public string GetValue()
+    {
+        return value;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Работа с int
+        IntStorage intStorage = new IntStorage();
+        intStorage.SetValue(42);
+        Console.WriteLine($"Int: {intStorage.GetValue()}");
+        
+        // Работа с string
+        StringStorage stringStorage = new StringStorage();
+        stringStorage.SetValue("Hello");
+        Console.WriteLine($"String: {stringStorage.GetValue()}");
+    }
+}
